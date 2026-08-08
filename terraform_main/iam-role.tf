8c92a31a4005aec9c2749bfe437a8c77eab6ac54
abhola <ankurhcl01@gmail.com>
2026-08-08 21:46:54 +0530

    vpc policy

--- terraform_main/iam-role.tf
+++ terraform_main/iam-role.tf
@@ -43,3 +43,19 @@ resource "aws_iam_role_policy" "terraform_s3_backend" {
 }
 EOF
 }
+
+######## Terrafom plan job fail because that role currently doesn't have permission to read VPC information. we used data "aws_vpc" "main" {"
+
+{
+  "Version": "2012-10-17",
+   "Statement": [
+     {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeVpcs"
+     ],
+      "Resource": "*"
+    }
+  ]
+}
+ 
