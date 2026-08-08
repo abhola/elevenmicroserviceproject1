5f06a3c0e59932843c2f887008b60abe654012fd
abhola <ankurhcl01@gmail.com>
2026-08-08 21:23:16 +0530

    policy

--- terraform_main/iam-role.tf
+++ terraform_main/iam-role.tf
@@ -15,6 +15,11 @@ resource "aws_iam_role" "iam-role" {
 }
 EOF
 }
+resource "aws_iam_role_policy" "terraform_s3_backend" {
+  name = "TerraformS3BackendAccess"
+  role = aws_iam_role.iam-role.id
+
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -34,14 +39,7 @@ EOF
       ],
       "Resource": "arn:aws:s3:::aluruarumullaa2985/eks/*"
     }
-   {
-     "Effect": "Allow",
-     "Action": [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject"
-         ],
-         "Resource": "arn:aws:s3:::aluruarumullaa2985/eks/*"
-   }
   ]
+}
+EOF
 }
