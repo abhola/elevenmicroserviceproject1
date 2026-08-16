601f838ecf966d9252c3b628393f5a956c1d7c9c
abhola <ankurhcl01@gmail.com>
2026-08-16 11:42:01 +0530

    sdsds

--- terraform_main/iam-role.tf
+++ terraform_main/iam-role.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role_policy" "terraform_s3_backend" {
       "Action": [
         "s3:ListBucket"
       ],
-      "Resource": "arn:aws:s3:::aluruarumullaa2985"
+      "Resource": "arn:aws:s3:::aluruarumullaa20"
     },
     {
       "Effect": "Allow",
@@ -37,7 +37,7 @@ resource "aws_iam_role_policy" "terraform_s3_backend" {
         "s3:PutObject",
         "s3:DeleteObject"
       ],
-      "Resource": "arn:aws:s3:::aluruarumullaa2985/eks/*"
+      "Resource": "arn:aws:s3:::aluruarumullaa20/eks/*"
     }
   ]
 }
